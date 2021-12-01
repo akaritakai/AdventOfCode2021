1e259c1380eaaf75c43e509e2264d9ee081efd28
Justin Kaufman <akaritakai@gmail.com>
2021-11-30 23:21:26 -0600

    Switch to arrays for day 1

--- src/main/java/net/akaritakai/aoc2021/Puzzle01.java
+++ src/main/java/net/akaritakai/aoc2021/Puzzle01.java
@@ -12,10 +12,10 @@ public class Puzzle01 extends AbstractPuzzle {
 
     @Override
     public String solvePart1() {
-        var depths = getPuzzleInput().lines().map(Integer::parseInt).toList();
+        var depths = getPuzzleInput().lines().mapToInt(Integer::parseInt).toArray();
         var count = 0;
-        for (var i = 1; i < depths.size(); i++) {
-            if (depths.get(i) > depths.get(i - 1)) {
+        for (var i = 1; i < depths.length; i++) {
+            if (depths[i] > depths[i - 1]) {
                 count++;
             }
         }
@@ -24,11 +24,11 @@ public class Puzzle01 extends AbstractPuzzle {
 
     @Override
     public String solvePart2() {
-        var depths = getPuzzleInput().lines().map(Integer::parseInt).toList();
+        var depths = getPuzzleInput().lines().mapToInt(Integer::parseInt).toArray();
         var count = 0;
-        var prevSum = depths.get(0) + depths.get(1) + depths.get(2);
-        for (var i = 3; i < depths.size(); i++) {
-            var sum = depths.get(i - 2) + depths.get(i - 1) + depths.get(i);
+        var prevSum = depths[0] + depths[1] + depths[2];
+        for (var i = 3; i < depths.length; i++) {
+            var sum = depths[i - 2] + depths[i - 1] + depths[i];
             if (sum > prevSum) {
                 count++;
             }
