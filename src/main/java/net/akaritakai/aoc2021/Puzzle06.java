cba39df3fb779a2cfc333435b94f39735454047d
Justin Kaufman <akaritakai@gmail.com>
2022-01-10 14:47:49 -0600

    Add description for day 6

--- src/main/java/net/akaritakai/aoc2021/Puzzle06.java
+++ src/main/java/net/akaritakai/aoc2021/Puzzle06.java
@@ -2,6 +2,16 @@ package net.akaritakai.aoc2021;
 
 import java.util.Arrays;
 
+/**
+ * In Day 6, we are given a counting problem. We have a number of fish which reproduce every 7 days. We are given an
+ * instance of fish and told their current timers for reproducing. We are asked to find how many fish there are after n
+ * days.
+ *
+ * Keeping each fish in memory is folly as the number of fish grow exponentially. Instead, we can keep count of how many
+ * fish are at a given timer each day and then figure out what the next day's count is.
+ *
+ * This gives us a complexity of O(n).
+ */
 public class Puzzle06 extends AbstractPuzzle {
     public Puzzle06(String puzzleInput) {
         super(puzzleInput);
