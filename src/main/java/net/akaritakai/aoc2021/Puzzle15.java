9d52c03a15532fd569e1051dca87171071cde8ae
Justin Kaufman <akaritakai@gmail.com>
2022-01-11 00:35:04 -0600

    Update Day 15

--- src/main/java/net/akaritakai/aoc2021/Puzzle15.java
+++ src/main/java/net/akaritakai/aoc2021/Puzzle15.java
@@ -7,7 +7,8 @@ import java.util.PriorityQueue;
  * In Day 15, we are given a weighted, directed graph of n vertices and told to find the shortest path from a given
  * start and end vertex.
  *
- * Using Dijkstra's algorithm, we can find the shortest path in O(n log n) time.
+ * Using Dijkstra's algorithm, we can find the shortest path in O(n log n) time, and we can use an A* admissible
+ * heuristic to reduce branching.
  */
 public class Puzzle15 extends AbstractPuzzle {
     public Puzzle15(String puzzleInput) {
@@ -62,8 +63,6 @@ public class Puzzle15 extends AbstractPuzzle {
             this.width = maxX;
         }
 
-        private int count;
-
         public int findMinRisk() {
             var comparator = new Comparator<int[]>() {
                 @Override
@@ -74,7 +73,6 @@ public class Puzzle15 extends AbstractPuzzle {
             var queue = new PriorityQueue<>(comparator);
             queue.add(new int[]{0, 0, dist[0][0] + width - 1 + height - 1});
             while (!queue.isEmpty()) {
-                count++;
                 var data = queue.poll();
                 var x = data[0];
                 var y = data[1];
@@ -82,35 +80,35 @@ public class Puzzle15 extends AbstractPuzzle {
                     return dist[y][x];
                 }
                 if (x > 0) {
-                    var dx = x - 1;
-                    var newDist = dist[y][x] + risk[y][dx];
-                    if (newDist < dist[y][dx]) {
-                        dist[y][dx] = newDist;
-                        queue.add(new int[]{dx, y, newDist + Math.abs(dx - width + 1) + Math.abs(y - height + 1)});
+                    var x2 = x - 1;
+                    var newDist = dist[y][x] + risk[y][x2];
+                    if (newDist < dist[y][x2]) {
+                        dist[y][x2] = newDist;
+                        queue.add(new int[]{x2, y, newDist + Math.abs(x2 - width + 1) + Math.abs(y - height + 1)});
                     }
                 }
                 if (y > 0) {
-                    var dy = y - 1;
-                    var newDist = dist[y][x] + risk[dy][x];
-                    if (newDist < dist[dy][x]) {
-                        dist[dy][x] = newDist;
-                        queue.add(new int[]{x, dy, newDist + Math.abs(x - width + 1) + Math.abs(dy - height + 1)});
+                    var y2 = y - 1;
+                    var newDist = dist[y][x] + risk[y2][x];
+                    if (newDist < dist[y2][x]) {
+                        dist[y2][x] = newDist;
+                        queue.add(new int[]{x, y2, newDist + Math.abs(x - width + 1) + Math.abs(y2 - height + 1)});
                     }
                 }
                 if (x < width - 1) {
-                    var dx = x + 1;
-                    var newDist = dist[y][x] + risk[y][dx];
-                    if (newDist < dist[y][dx]) {
-                        dist[y][dx] = newDist;
-                        queue.add(new int[]{dx, y, newDist + Math.abs(dx - width + 1) + Math.abs(y - height + 1)});
+                    var x2 = x + 1;
+                    var newDist = dist[y][x] + risk[y][x2];
+                    if (newDist < dist[y][x2]) {
+                        dist[y][x2] = newDist;
+                        queue.add(new int[]{x2, y, newDist + Math.abs(x2 - width + 1) + Math.abs(y - height + 1)});
                     }
                 }
                 if (y < height - 1) {
-                    var dy = y + 1;
-                    var newDist = dist[y][x] + risk[dy][x];
-                    if (newDist < dist[dy][x]) {
-                        dist[dy][x] = newDist;
-                        queue.add(new int[]{x, dy, newDist + Math.abs(x - width + 1) + Math.abs(dy - height + 1)});
+                    var y2 = y + 1;
+                    var newDist = dist[y][x] + risk[y2][x];
+                    if (newDist < dist[y2][x]) {
+                        dist[y2][x] = newDist;
+                        queue.add(new int[]{x, y2, newDist + Math.abs(x - width + 1) + Math.abs(y2 - height + 1)});
                     }
                 }
             }
