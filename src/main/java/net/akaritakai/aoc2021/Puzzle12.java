89cd2d9ca85767ee18397725c8943093d35461fd
Justin Kaufman <akaritakai@gmail.com>
2022-01-10 23:44:23 -0600

    Day 12 performance improvements

--- src/main/java/net/akaritakai/aoc2021/Puzzle12.java
+++ src/main/java/net/akaritakai/aoc2021/Puzzle12.java
@@ -9,10 +9,15 @@ import java.util.*;
  * The problem is amenable to simple recursion to count the paths.
  */
 public class Puzzle12 extends AbstractPuzzle {
-    private final Map<String, Set<String>> edges = new HashMap<>();
+    private final int start;
+    private final int end;
+    private final boolean[] smallCaves;
+    private final boolean[][] edges;
+    private final int[] path;
 
     public Puzzle12(String puzzleInput) {
         super(puzzleInput);
+        var edges = new HashMap<String, Set<String>>();
         for (var line : getPuzzleInput().split("\n")) {
             var parts = line.split("-");
             var from = parts[0];
@@ -20,6 +25,22 @@ public class Puzzle12 extends AbstractPuzzle {
             edges.computeIfAbsent(from, s -> new HashSet<>()).add(to);
             edges.computeIfAbsent(to, s -> new HashSet<>()).add(from);
         }
+        var caves = edges.keySet().stream().sorted().toArray(String[]::new);
+        this.edges = new boolean[caves.length][caves.length];
+        for (var i = 0; i < caves.length; i++) {
+            for (var j = 0; j < caves.length; j++) {
+                this.edges[i][j] = edges.get(caves[i]).contains(caves[j]);
+            }
+        }
+        start = Arrays.binarySearch(caves, "start");
+        end = Arrays.binarySearch(caves, "end");
+        smallCaves = new boolean[caves.length];
+        for (var i = 0; i < caves.length; i++) {
+            if (caves[i].charAt(0) >= 'a') {
+                smallCaves[i] = true;
+            }
+        }
+        path = new int[caves.length];
     }
 
     @Override
@@ -29,34 +50,32 @@ public class Puzzle12 extends AbstractPuzzle {
 
     @Override
     public String solvePart1() {
-        return String.valueOf(countPaths("start", new LinkedList<>(), true));
+        return String.valueOf(countPaths(start, true));
     }
 
     @Override
     public String solvePart2() {
-        return String.valueOf(countPaths("start", new LinkedList<>(), false));
+        return String.valueOf(countPaths(start, false));
     }
 
-    private int countPaths(String cave, LinkedList<String> path, boolean seenTwice) {
-        if (cave.equals("end")) {
+    private int countPaths(int cave, boolean seenTwice) {
+        if (cave == end) {
             return 1;
         }
-        if (isSmallCave(cave) && path.contains(cave)) {
-            if (seenTwice || cave.equals("start")) {
+        if (smallCaves[cave] && path[cave] > 0) {
+            if (seenTwice || cave == start) {
                 return 0;
             }
             seenTwice = true;
         }
-        path.addLast(cave);
+        path[cave]++;
         var count = 0;
-        for (var next : edges.get(cave)) {
-            count += countPaths(next, path, seenTwice);
+        for (var next = 0; next < edges[cave].length; next++) {
+            if (edges[cave][next]) {
+                count += countPaths(next, seenTwice);
+            }
         }
-        path.removeLast();
+        path[cave]--;
         return count;
-    }
-
-    private static boolean isSmallCave(String cave) {
-        return cave.charAt(0) >= 'a';
     }
 }
